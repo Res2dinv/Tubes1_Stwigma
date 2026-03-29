efaf9c2bd53c6aa10c38a4597ee66ca2eab2895f
danielanindto <danielnugroho900@gmail.com>
2026-03-29 23:31:53 +0700

    refactor : change package and import name

--- src/alternative_bots_2/RobotPlayer.java
+++ src/alternative_bots_2/RobotPlayer.java
@@ -1,4 +1,4 @@
-package control;
+package alternative_bots_2;
 
 import java.util.Random;
 
--- src/main_bot/RobotPlayer.java
+++ src/main_bot/RobotPlayer.java
@@ -1,6 +1,15 @@
-package endthisgng;
+package main_bot;
 
-import battlecode.common.*;
+import battlecode.common.Clock;
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapInfo;
+import battlecode.common.MapLocation;
+import battlecode.common.PaintType;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.UnitType;
+
 
 public class RobotPlayer {
 
